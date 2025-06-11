5bacbaff18ec82a6f8199272507994efa1ce8e91
Jeremy <jerimielid7@gmail.com>
2025-06-11 16:34:02 +0300

    Update MainServer.java
    Revised to access the database

--- src/MainServer.java
+++ src/MainServer.java
@@ -1,26 +1,16 @@
-import java.rmi.*;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.util.List;
 
 public class MainServer {
-    public static void main(String[] args){
-        try{
-            LocateRegistry.createRegistry(1099);
-            OrderingService service = new OrderingServiceImp() {
-                public String placeOrder(int customerId, int branchId, List<OrderItem> items) {
-                    return "";
-                }
-
-                public String generateReport() throws RemoteException {
-                    return "";
-                }
-            };
+    public static void main(String[] args) {
+        try {
+            LocateRegistry.createRegistry(1099); // Default RMI port
+            OrderingService service = new OrderingServiceImp();
             Registry registry = LocateRegistry.getRegistry();
             registry.rebind("OrderService", service);
-            System.out.println("Server ready");
-        } catch (Exception error) {
-            error.printStackTrace();
+            System.out.println("RMI Server is running...");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
